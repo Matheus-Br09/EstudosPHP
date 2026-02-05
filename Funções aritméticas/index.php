a95fa7d4876a776ca2044b3b969d0cdf6c2e6044
Matheus-Br09 <matheusps3.br06@gmail.com>
2026-02-05 15:23:27 -0300

    Operações matemáticas - 05/02

---
+++ Funções aritméticas/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções Ariméticas</title>
+</head>
+<body>
+    
+    <?php 
+        //Número absoluto.
+        // $r = abs(-2000);
+        // echo "A resposta é $r";
+
+        //Converter bases (binário, octal, decimal e hexadecimal)
+        // $r = base_convert(1011, 2, 8);
+        // echo $r;
+
+        //Funções para arredondamento
+        // ceil(), floor(), round();
+
+        //Calcular hipotenusa.
+        // hypot();
+
+        //Divisor inteiro = intdiv();
+        // $r = intdiv(5, 2);
+        // print("A resposta é $r")
+
+        //Mínimo e máximo
+        // $r = min(5, 2) -> 2
+        // $r = max(4, 7) -> 7
+
+        //Valor de pi -> pi(); ou a constante M_PI;
+
+        //Potência sem usar operador -> pow();
+
+        //Funções trigonométricas -> sin(), cos(), tan();
+
+        //Raiz quadrada -> sqrt() ou 81 ** (1/2)
+        $r = sqrt(81);
+        echo $r;
+    ?>
+</body>
+</html>
