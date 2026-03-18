82fbe310d525e0032b03ff8506411b938f03c3ac
Matheus-Br09 <matheusps3.br06@gmail.com>
2026-03-18 17:38:28 -0300

    teste de alteração - 18/03

--- Obtendo dados de formulários/cad.php
+++ Obtendo dados de formulários/cad.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <h1>Resultado do processamento</h1>
@@ -14,9 +16,10 @@
         <?php 
             $nome = $_GET["nome"] ?? "desconhecido";
             $sobrenome = $_GET["sobrenome"] ?? "desconhecido";
-            echo "Olá $nome $sobrenome!> Seja bem vindo, como estás?";
+            echo "Olá $nome $sobrenome! Seja bem vindo, como estás?";
         ?>
         <p><a href="javascript:history.go(-1)">Voltar a página anterior!</a></p>
     </main>
 </body>
+
 </html>
